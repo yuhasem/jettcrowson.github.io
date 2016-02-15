b5fc37e888b87a3ef42f7e1664ebddd6c91d7e7c
Jett Crowson <jettcrowson@gmail.com>
2016-02-15 10:30:26 -0700

    try

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var authKey = URL.substr(URL.indexOf("=") + 1);
 console.log("The auth key is " + authKey);
 
 var followers = $.get("https://api.instagram.com/v1/users/self/follows?access_token=ACCESS-TOKEN", function(data){
-
 	console.log(followers);
-
 });
 });
