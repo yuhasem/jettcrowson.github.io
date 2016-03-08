8c1c3527fbdc8591ba9732efe6bc29293aafaad8
Jett Crowson <jettcrowson@gmail.com>
2016-03-08 16:42:20 -0700

    Added the ability to submit commands (they don't do anything yet) and tidied the code

--- CMD++/js/index.js
+++ CMD++/js/index.js
@@ -1,26 +1,42 @@
-$(document).ready(function(){
-  respond("Welcome to CMD++");
-  respond("Your goal here is to mine data.");
-  checkForSave();
+/**
+TODO List:
+- 
+**/
+//All game values and functions will be stored here.
+var CMD = {
+  //The currency variables
+  "data": 0,
+  "money": 0,
+  //Creates a new line in the CMD
+  "respond": function(text) {
+    //Add a new table row, used as a line in the CMD
+    $("#responses").append("<tr class='response'><td class='response'>> " + text + "</td></tr>");
+  },
+  //Gets rid of all of the responses
+  "clear": function() {
+    $("#responses").remove();
+  },
+  //When the user enters a command, this is run to check if they typed anything, and if they did, submit it to CMD.runCommand().
+  "command": function() {
+    if ($("#input").val() != "") {
+      //Submit the command
+      CMD.runCommand($("#input").val());
+      $("#input").val("");
+    }
+  },
+  //Check if the command exists, and if it does, run it.
+  "runCommand": function(commandToRun) {
+    CMD.respond(commandToRun);
+  }
+};
+//Check if the Enter key was pressed
+$(document).keypress(function(e) {
+  if (e.which == 13) {
+    CMD.command();
+  }
 });
-function checkForSave(){
-  respond("Checking for save...");
-//TODO: CHECK FOR SAVE
-  respond("No save found. Starting new game...");
-  newGame();
-}
-function newGame(){
-  respond("Initializing database",500);
-  respond("Firing up",1000);
-  respond("Pinging servers",2000);
-  respond("Configuring OS",3000);
-  respond("Ready",3250);
-  respond("Clearing",4000);
-  clear();
-}
-function respond(text, delay){
-    setTimeout(function(){
-    $("#responses").append("<tr><td>> "+text+"</td></tr>");
-}, delay);
-  
-}
+//Called when the user first enters the page. 
+$(document).ready(function() {
+  CMD.respond("Welcome to CMD++");
+  CMD.respond("Your goal here is to mine data.");
+});
