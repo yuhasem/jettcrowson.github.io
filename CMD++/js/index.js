d88e195ff752abb72ec77eda34b3148541b92600
Jett C <jettcrowson@gmail.com>
2016-03-09 16:39:15 -0700

    Added ability to actually mine data

--- CMD++/js/index.js
+++ CMD++/js/index.js
@@ -8,15 +8,16 @@ var CMD = {
   //The currency variables
   data: 0,
   money: 0,
+  increment: 1,
+  autoIncrement: 0,
   //Creates a new line in the CMD
   respond: function(text) {
     //Add a new table row, used as a line in the CMD
     $("#responses").append("<tr class='response'><td class='response'>> " + text + "</td></tr>");
   },
-  //Gets rid of all of the responses
-  clear: function() {
-    $("#responses").remove();
-  },
+  gameLoop: setInterval(function(){
+    CMD.addData(CMD.autoIncrement);
+}, 1000),
   //When the user enters a command, this is run to check if they typed anything, and if they did, submit it to CMD.runCommand().
   command: function() {
     if ($("#input").val() != "") {
@@ -34,27 +35,48 @@ var CMD = {
       CMD.respond("Command not found.");
     } else {
       for (var i = 0; i < CMD.commandList.length; i++) {
-        //Check if command exists
-        if (commandToRun === CMD.commandList[i]) {
+        //Check if command exists and is unlocked
+        if (commandToRun === CMD.commandList[i] && CMD.commandUnlocked[i] === true) {
           CMD.commands[commandToRun]();
+          //If the command DOES exist, but is not unlocked
+        } else if (commandToRun === CMD.commandList[i] && CMD.commandUnlocked[i] === false) {
+          CMD.respond("Command not found.");
         }
 
       }
     }
 
   },
+  //Update the data count
+  update: function() {
+    $("#dataCount").html(CMD.data);
+  },
+  //Add data
+  addData: function(amount) {
+    CMD.data += amount;
+    CMD.update();
+  },
   //LIST ALL COMMANDS HERE, OTHERWISE THEY WILL RETURN AS NOT EXISTING
-  commandList: ["help", "mineData", "save"],
+  commandList: ["help", "mineData", "save", "autoMine"],
+  //SET EACH FUNCTION TO WHETHER IT IS UNLOCKED
+  commandUnlocked: [true, true, true, false],
   //Command object stores all game functions, not the actual engine functions
   commands: {
     help: function() {
       CMD.respond("########################################");
       CMD.respond("List of commands:");
-      CMD.respond(CMD.commandList.join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"));
+      var availableCommands = [];
+      for (var r = 0; r < CMD.commandList.length; r++) {
+        if (CMD.commandUnlocked[r] === true) {
+          availableCommands.push(CMD.commandList[r]);
+        }
+      }
+      CMD.respond(availableCommands.join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"));
       CMD.respond("########################################");
     },
     mineData: function() {
       CMD.respond("Data mined.");
+      CMD.addData(CMD.increment);
     },
     save: function() {
       CMD.respond("TODO add save function");
