ea919ba4cb6585732b3f37287d10ae3bb934d862
Jett Crowson <jettcrowson@gmail.com>
2016-03-08 20:43:51 -0700

    Made some commands do something. Now incredibly easy to add commands
    Add commands to the CMD.commandList[] and then the actual function to
    the CMD.commands{}

--- CMD++/js/index.js
+++ CMD++/js/index.js
@@ -1,38 +1,71 @@
 /**
 TODO List:
-- 
+- Add different amounts of data depending on your upgrades
 **/
+
 //All game values and functions will be stored here.
 var CMD = {
   //The currency variables
-  "data": 0,
-  "money": 0,
+  data: 0,
+  money: 0,
   //Creates a new line in the CMD
-  "respond": function(text) {
+  respond: function(text) {
     //Add a new table row, used as a line in the CMD
     $("#responses").append("<tr class='response'><td class='response'>> " + text + "</td></tr>");
   },
   //Gets rid of all of the responses
-  "clear": function() {
+  clear: function() {
     $("#responses").remove();
   },
   //When the user enters a command, this is run to check if they typed anything, and if they did, submit it to CMD.runCommand().
-  "command": function() {
+  command: function() {
     if ($("#input").val() != "") {
       //Submit the command
       CMD.runCommand($("#input").val());
+      //Reset the imput field
       $("#input").val("");
     }
   },
   //Check if the command exists, and if it does, run it.
-  "runCommand": function(commandToRun) {
-    CMD.respond(commandToRun);
+  runCommand: function(commandToRun) {
+
+    //REMEMBER: ALWAYS ADD YOUR COMMANDS TO THE COMMANDLIST ARRAY AND THE COMMAND OBJECT
+    if (CMD.commandList.indexOf(commandToRun) === -1) {
+      CMD.respond("Command not found.");
+    } else {
+      for (var i = 0; i < CMD.commandList.length; i++) {
+        //Check if command exists
+        if (commandToRun === CMD.commandList[i]) {
+          CMD.commands[commandToRun]();
+        }
+
+      }
+    }
+
+  },
+  //LIST ALL COMMANDS HERE, OTHERWISE THEY WILL RETURN AS NOT EXISTING
+  commandList: ["help", "mineData", "save"],
+  //Command object stores all game functions, not the actual engine functions
+  commands: {
+    help: function() {
+      CMD.respond("########################################");
+      CMD.respond("List of commands:");
+      CMD.respond(CMD.commandList.join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"));
+      CMD.respond("########################################");
+    },
+    mineData: function() {
+      CMD.respond("Data mined.");
+    },
+    save: function() {
+      CMD.respond("TODO add save function");
+    }
   }
 };
 //Check if the Enter key was pressed
 $(document).keypress(function(e) {
   if (e.which == 13) {
     CMD.command();
+    $('#cmdWindow').scrollTop($('#cmdWindow')[0].scrollHeight);
   }
 });
 //Called when the user first enters the page. 
